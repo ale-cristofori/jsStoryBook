8d30649a565c2b39ba5de7d594de22e2018fa2ff
Alessandro Cristofori <compatt4@gmail.com>
2016-07-30 23:50:36 +0100

    refinement Queen class: conversation method

--- js/Royals.js
+++ js/Royals.js
@@ -58,7 +58,7 @@ var Royals = function(){
 			return true;
 		}
 	};	
-}
+};
 
 /**
 * King class:
@@ -79,7 +79,7 @@ function King() {
 		alert('deffo my dear friend, I love horses, let''s go')
 		};
 	}
-}
+};
 
 King.prototype = new Royals();
 
@@ -88,11 +88,90 @@ King.prototype = new Royals();
 * Royals subclass, a female royal
 * with particular activities and properties
 **/
-function Queen(){
+function Queen(){	
+	var answersObj = new Object();
+	var evaluateAnswers = function(sumAnswers) {
+		if (sumAnswers <= 7) {
+			alert("this is not good my friend");
+		} else {
+			alert("this is good you might win the game");
+		}
+	}
+	var handleConversation = function (i = 1) {
+		switch (i) {
+			case 1:
+				alert("So my subject, what do you think about last week Mademoiselle Rupert's party? Did you enjoy it?");
+				var radioButtonsFirst = $([
+					"<form id='questions'>", 
+					"<input type='radio' value='0' name='answer-1'>party?!? I was not invited. <br>",
+					"<input type='radio' value='3' name='answer-1'>The party was great and Mademoiselle, what a pretty and mannered woman!! <br>",
+					"<input type='radio' value='1' name='answer-1'>I was there but I the best think to do in that party was to drink, to forget to be there!!ah ah <br><br>",
+					"<input type='button' id='answer-button-1' value='Answer'><br>",
+					"</form>"
+				].join("\n"));	
+				var yourResponseFirst = $('#radio-buttons').append(radioButtonsFirst);
+				$(document).on('click', '#answer-button-1', function(){
+					var answerNum = $('#questions > input:checked').attr('name');
+					var answerVal = $('#questions > input:checked').attr('value');
+					answersObj[answerNum] = answerVal;
+					$('#questions').remove();
+					handleConversation(2);
+				});
+				break;
+			case 2:
+				alert("Inferior, What do you think of the last laws approved by Minister Colbert?");
+				var radioButtonsSecond = $([
+					"<form id='questions'>", 
+					"<input type='radio' value='0' name='answer-2'>Totally agree with his decisions to help the poorest people<br>",
+					"<input type='radio' value='2' name='answer-2'>Absolutely rubbish, Does he want poor people to become part of us?<br>",
+					"<input type='radio' value='3' name='answer-2'>I know him very well and it was me the person behind the policy!!<br><br>",
+					"<input type='button' id='answer-button-2' value='Answer'><br>",
+					"</form>"
+				].join("\n"));
+				var yourResponseSecond = $('#radio-buttons').append(radioButtonsSecond);
+				$(document).on('click', '#answer-button-2', function(){
+					var answerNum = $('#questions > input:checked').attr('name');
+					var answerVal = $('#questions > input:checked').attr('value');
+					answersObj[answerNum] = answerVal;
+					$('#questions').remove();
+					handleConversation(3);
+				});
+				break;
+			case 3:
+				alert("Inferior, And what about Madame de Pompadour, do you think she should be put out of the way");
+				var radioButtonsThird = $([
+					"<form id='questions'>", 
+					"<input type='radio' value='0' name='answer-3'>No, although she might come across as ambigous I don't think she is a threat<br>",
+					"<input type='radio' value='3' name='answer-3'>Of course Madam, here I have some poison in case you need it<br>",
+					"<input type='radio' value='5' name='answer-3'>Yes, we should create a scandal to sully his reputation<br><br>",
+					"<input type='button' id='answer-button-3' value='Answer'><br>",
+					"</form>"
+				].join("\n"));
+				var yourResponseThird = $('#radio-buttons').append(radioButtonsThird);
+				var yourResponseSecond = $('#radio-buttons').append(radioButtonsSecond);
+				$(document).on('click', '#answer-button-3', function(){
+					var answerNum = $('#questions > input:checked').attr('name');
+					var answerVal = $('#questions > input:checked').attr('value');
+					answersObj[answerNum] = answerVal;
+					$('#questions').remove();
+					var sumAnswers = 0;
+					for (key in answersObj) {
+						sumAnswers += Number(answersObj[key]);
+					}
+					evaluateAnswers(sumAnswers);
+				});
+				break;
+			default:
+			alert("ERROR");
+			break;
+		};
+	}
 	this.name = "Reine Marie Antoinette";
 	this.age = 26;
 	this.sex = 'female';
+	this.conversation = handleConversation;
 	//other functions here
-}
+};
 
 Queen.prototype = new Royals();
+
